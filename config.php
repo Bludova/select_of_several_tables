d4493dfa3466da194bd5afb8d11e40d88f78edc4
Bludova <32148601+Bludova@users.noreply.github.com>
2017-11-09 03:39:37 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,5 @@
 <?php
-// $host = 'localhost'; 
+$host = 'localhost'; 
 $db = 'global';
-// $user = 'bludova';
-// $password = 'neto1261';
-// mysql_query($host "'utf8'"); 
-// mysql_query("SET CHARACTER SET 'utf8'");
-// mysql_query("SET SESSION collation_connection = 'utf8_general_ci'");
-
-$host = 'localhost';  
-// $db = 'tasks';
-$user = 'root';
+$user = 'bludova';
 $password = '';
